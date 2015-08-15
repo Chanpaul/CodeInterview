43bf1b541a800462b6955d0308803dbfda784a73
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-15 16:40:22 -0400

    add recursion solution

--- src/Interview/BuySellStockOnce.java
+++ src/Interview/BuySellStockOnce.java
@@ -9,6 +9,29 @@ import java.util.Random;
  * presented; one is a recursion version and the other is the linear scan version.
  */
 public class BuySellStockOnce {
+    public int bestRecursion(int[] stocks) {
+        if (stocks == null || stocks.length < 2) {
+            return 0;
+        }
+        return maxProfit(stocks, 0, stocks.length - 1);
+    }
+
+    // The recursion solution takes O(n*logn) time.
+    private int maxProfit(int[] stocks, int lo, int hi) {
+        if (lo >= hi) return 0;
+        int mid = lo + (hi - lo) / 2;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = lo; i <= mid; i ++) {
+            min = Math.min(min, stocks[i]);
+        }
+        for (int i = mid + 1; i <= hi; i ++) {
+            max = Math.max(max, stocks[i]);
+        }
+        int max1 = maxProfit(stocks, lo, mid);
+        int max2 = maxProfit(stocks, mid + 1, hi);
+        return Math.max(Math.max(max - min, max1), max2);
+    }
 
     // linear scan takes O(n) time
     public int bestLinearScan(int[] stocks) {
@@ -33,5 +56,6 @@ public class BuySellStockOnce {
         System.out.println(Arrays.toString(stocks));
         BuySellStockOnce bss = new BuySellStockOnce();
         System.out.println(bss.bestLinearScan(stocks));
+        System.out.println(bss.bestRecursion(stocks));
     }
 }
