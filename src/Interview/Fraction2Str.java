5be1b0dc6a8e496f23a52112945bf71f8e658937
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 13:42:34 -0400

    add description

--- src/Interview/Fraction2Str.java
+++ src/Interview/Fraction2Str.java
@@ -5,6 +5,16 @@ import java.util.Map;
 
 /**
  * Created by yongyangyu on 12/16/14.
+ * Given two integers representing the numerator and denominator of a fraction,
+ * return the fraction in string format.
+ *
+ * If the fractional part is repeating, enclose the repeating part in parentheses.
+ *
+ * For example,
+ *
+ * Given numerator = 1, denominator = 2, return "0.5".
+ * Given numerator = 2, denominator = 1, return "2".
+ * Given numerator = 2, denominator = 3, return "0.(6)".
  */
 public class Fraction2Str {
     public static String fractionToDecimal(int numerator, int denominator) {
