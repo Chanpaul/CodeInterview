7646b3ad73379205438aacd8e7313a73399a0f89
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 14:14:37 -0400

    add description

--- src/Interview/StrIsomorphic.java
+++ src/Interview/StrIsomorphic.java
@@ -4,6 +4,20 @@ import java.util.HashMap;
 
 /**
  * Created by yongyangyu on 11/19/14.
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while
+ * preserving the order of characters. No two characters may map to the same character but
+ * a character may map to itself.
+ *
+ * For example,
+ * Given "egg", "add", return true.
+ *
+ * Given "foo", "bar", return false.
+ *
+ * Given "paper", "title", return true.
  */
 public class StrIsomorphic {
     public static boolean testIsomorphic(String s1, String s2) {
