d991b045e492656cba52ea85815fd8818d57bad5
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 14:20:18 -0400

    add description

--- src/Interview/WordBreak.java
+++ src/Interview/WordBreak.java
@@ -4,6 +4,16 @@ import java.util.*;
 
 /**
  * Created by yongyangyu on 1/21/15.
+ * Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where
+ * each word is a valid dictionary word.
+ *
+ * Return all such possible sentences.
+ *
+ * For example, given
+ * s = "catsanddog",
+ * dict = ["cat", "cats", "and", "sand", "dog"].
+ *
+ * A solution is ["cats and dog", "cat sand dog"].
  */
 public class WordBreak {
     public static List<String> wordBreak(String s, Set<String> dict) {
