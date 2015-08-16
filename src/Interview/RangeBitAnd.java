88b8fa3da3fb4b00789d6506a27cfef1ccd5b03b
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 13:58:33 -0400

    add description

--- src/Interview/RangeBitAnd.java
+++ src/Interview/RangeBitAnd.java
@@ -2,6 +2,10 @@ package Interview;
 
 /**
  * Created by yongyangyu on 4/16/15.
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647,
+ * return the bitwise AND of all numbers in this range, inclusive.
+ *
+ * For example, given the range [5, 7], you should return 4.
  */
 public class RangeBitAnd {
     public static int rangeBitwiseAnd(int m, int n) {
