fd28bfab393b3019a60c0aebf52b29f3e4c719de
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 13:36:16 -0400

    add description

--- src/Interview/EditDistance.java
+++ src/Interview/EditDistance.java
@@ -2,6 +2,14 @@ package Interview;
 
 /**
  * Created by yongyangyu on 8/3/15.
+ * Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2.
+ * (each operation is counted as 1 step.)
+ *
+ * You have the following 3 operations permitted on a word:
+ *
+ * a) Insert a character
+ * b) Delete a character
+ * c) Replace a character
  */
 public class EditDistance {
     public static int dist(String s1, String s2) {
