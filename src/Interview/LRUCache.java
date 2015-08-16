24902e65b82daf0c20ac1c82696cd88746ff84fd
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 13:47:04 -0400

    add description

--- src/Interview/LRUCache.java
+++ src/Interview/LRUCache.java
@@ -4,6 +4,14 @@ import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 /**
  * Created by yongyangyu on 12/8/14.
+ * Design and implement a data structure for Least Recently Used (LRU) cache.
+ * It should support the following operations: get and set.
+ *
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache,
+ * otherwise return -1.
+ * set(key, value) - Set or insert the value if the key is not already present.
+ * When the cache reached its capacity, it should invalidate the least recently used item before
+ * inserting a new item.
  */
 public class LRUCache {
     myCache<Integer, Integer> cache;
