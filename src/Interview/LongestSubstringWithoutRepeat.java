d01a3a68dd2dc965c15b494209ee8a2ca69566ab
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 13:46:08 -0400

    add description

--- src/Interview/LongestSubstringWithoutRepeat.java
+++ src/Interview/LongestSubstringWithoutRepeat.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 /**
  * Created by yongyangyu on 2/10/15.
+ * Given a string, find the length of the longest substring without repeating characters.
+ * For example, the longest substring without repeating letters for "abcabcbb" is "abc",
+ * which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
  */
 public class LongestSubstringWithoutRepeat {
     public static int lengthOfLongestSubstring(String s) {
