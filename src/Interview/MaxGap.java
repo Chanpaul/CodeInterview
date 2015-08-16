92d5087698db5548d35c8f8fbc4137bf49ef6cc3
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-16 13:48:13 -0400

    add description

--- src/Interview/MaxGap.java
+++ src/Interview/MaxGap.java
@@ -8,6 +8,15 @@ import java.util.List;
 
 /**
  * Created by yongyangyu on 12/14/14.
+ * Given an unsorted array, find the maximum difference between the successive
+ * elements in its sorted form.
+ *
+ * Try to solve it in linear time/space.
+ *
+ * Return 0 if the array contains less than 2 elements.
+ *
+ * You may assume all elements in the array are non-negative integers and fit in the 32-bit
+ * signed integer range.
  */
 public class MaxGap {
     public static int maximumGap(int[] num) {
