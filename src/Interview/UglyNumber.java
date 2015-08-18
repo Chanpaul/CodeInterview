08bdd2637b17c92ea0818e9d670a3a53cb0225e0
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-18 18:15:03 -0400

    change format

--- src/Interview/UglyNumber.java
+++ src/Interview/UglyNumber.java
@@ -15,16 +15,13 @@ public class UglyNumber {
         while (num % 5 == 0) {
             num /= 5;
         }
-        if (num == 1) return true;
         while (num % 3 == 0) {
             num /= 3;
         }
-        if (num == 1) return true;
         while (num % 2 == 0) {
             num /= 2;
         }
-        if (num == 1) return true;
-        return false;
+        return num == 1 ? true : false;
     }
 
     /**
