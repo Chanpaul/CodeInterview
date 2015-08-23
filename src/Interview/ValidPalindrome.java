e70cf18e1ffa4488246602e153ac53b1bcb03aff
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-23 16:24:55 -0400

    add valid palindrome

--- src/Interview/ValidPalindrome.java
+++ src/Interview/ValidPalindrome.java
@@ -13,9 +13,8 @@ public class ValidPalindrome {
         if (s == null || s.length() == 0) return true;
         int i = 0, j = s.length() - 1;
         while (i < j) {
-            while (i < s.length() && !Character.isLetterOrDigit(s.charAt(i))) i++;
-            while (j >= 0 && !Character.isLetterOrDigit(s.charAt(j))) j--;
-            if (i >= j) return true;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
             if (Character.toUpperCase(s.charAt(i)) != Character.toUpperCase(s.charAt(j))) return false;
             else {
                 i++;
