3ea95b82ace6b7c3c8039ceb070dbcbc60348380
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-23 16:22:06 -0400

    add valid palindrome

---
+++ src/Interview/ValidPalindrome.java
@@ -0,0 +1,33 @@
+package Interview;
+
+/**
+ * Created by yongyangyu on 8/23/15.
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ *
+ * For example,
+ * "A man, a plan, a canal: Panama" is a palindrome.
+ * "race a car" is not a palindrome.
+ */
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < s.length() && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (j >= 0 && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if (i >= j) return true;
+            if (Character.toUpperCase(s.charAt(i)) != Character.toUpperCase(s.charAt(j))) return false;
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(vp.isPalindrome("race a car"));
+    }
+}
