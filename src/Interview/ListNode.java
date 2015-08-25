c067198452f4e982236cab87a672c80e4ed09f6e
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-25 15:02:21 -0400

    add reverse recursion

--- src/Interview/ListNode.java
+++ src/Interview/ListNode.java
@@ -80,6 +80,15 @@ public class ListNode {
         return head;
     }
 
+    // recursive version
+    public static ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
     public static void main(String[] args) {
         int[] testArray = {1, 2, 6, 3, 4, 5, 6};
         ListNode head = create(testArray);
@@ -90,5 +99,7 @@ public class ListNode {
         printListNodes(head);
         System.out.println();
         printListNodes(addTwoNumbers(l1, l2));
+        System.out.print('\n');
+        printListNodes(reverse(head));
     }
 }
