a0996ef3c29dbd74f8d6e674d029caf0d536c854
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-24 22:45:03 -0400

    refactor rabin karp

--- src/Interview/RabinKarp.java
+++ src/Interview/RabinKarp.java
@@ -2,37 +2,39 @@ package Interview;
 
 /**
  * Created by yongyangyu on 12/3/14.
+ * Refactored to better compute powers of the base.
  */
 public class RabinKarp {
-    public static int strStr(String T, String P) {
-        int base = 29;
-        long phash = 0;
-        if (T.length() < P.length()) {
-            return -1;
+    static final int base = 26;
+    static final int mod = 997;
+    public static int strStr(String haystack, String needle) {
+        if (needle.length() > haystack.length()) return -1;  // needle cannot be a substring
+        int hayHash = 0, nhash = 0;
+        int power = 1;
+        for (int i = 0; i < needle.length(); i ++) {
+            power = i != 0 ? power * base % mod : 1;
+            hayHash = (hayHash * base + haystack.charAt(i)) % mod;
+            nhash = (nhash * base + needle.charAt(i)) % mod;
         }
-        long thash = 0;
-        for (int i = 0; i < P.length(); i ++) {
-            phash = phash * base + new Character(P.charAt(i)).hashCode();
-            thash = thash * base + new Character(T.charAt(i)).hashCode();
-        }
-        if (thash == phash) {
-            return 0;
-        }
-        else {
-            for (int i = 1; i < T.length(); i ++) {
-                thash -= new Character(T.charAt(i - 1)).hashCode() * Math.pow(base, P.length() - 1);
-                thash = thash * base + new Character(T.charAt(i + P.length() - 1)).hashCode();
-                if (thash == phash) {
-                    return i;
-                }
+
+        for (int i = needle.length(); i < haystack.length(); i ++) {
+            if (hayHash == nhash && needle.equals(haystack.substring(i - needle.length(), i))) {
+                return i - needle.length();
             }
+            hayHash -= (power * haystack.charAt(i - needle.length())) % mod;
+            if (hayHash < 0) hayHash += mod;
+            hayHash = (hayHash * base + haystack.charAt(i)) % mod;
         }
+        if (hayHash == nhash && needle.equals(haystack.substring(haystack.length() - needle.length()))) {
+            return haystack.length() - needle.length();
+        }
+
         return -1;
     }
 
     public static void main(String[] args) {
-        String T = "bacbabababacaab";
-        String P = "ababaca";
-        System.out.println(strStr(T, P));
+        String haystack = "bacbabababacaab";
+        String needle = "ababaca";
+        System.out.println(strStr(haystack, needle));
     }
 }
