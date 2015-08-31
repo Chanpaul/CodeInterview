254ae96061aad4cfbede9426458659ef9f7e8899
Yongyang Yu <yuyongyang800@gmail.com>
2015-08-31 10:26:16 -0400

    add postorder-inorder recover

--- src/Interview/RecoverBinaryTree.java
+++ src/Interview/RecoverBinaryTree.java
@@ -15,7 +15,7 @@ public class RecoverBinaryTree {
         }
         return buildFromPreorderInorder(preorder, 0, preorder.length - 1, 0, inorder.length - 1, inMap);
     }
-
+    // preorder[preStart : preEnd] included and inorder[inStart, inEnd] included
     private TreeNode buildFromPreorderInorder(int[] preorder, int preStart, int preEnd,
                                               int inStart, int inEnd, Map<Integer, Integer> inMap) {
         if (preStart > preEnd || inStart > inEnd) return null;
@@ -30,6 +30,35 @@ public class RecoverBinaryTree {
 
     }
 
+    public TreeNode recoverFromPostOrderAndInorder(int[] postorder, int[] inorder) {
+        Map<Integer, Integer> inMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i ++) {
+            inMap.put(inorder[i], i);
+        }
+        int start = 0, end = postorder.length - 1;
+        while (start < end) {
+            int tmp = postorder[start];
+            postorder[start] = postorder[end];
+            postorder[end] = tmp;
+            start ++;
+            end --;
+        }
+        return buildFromPostorderInorder(postorder, 0, postorder.length - 1, 0, inorder.length - 1, inMap);
+    }
+
+    private TreeNode buildFromPostorderInorder(int[] postorder, int postStart, int postEnd,
+                                               int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if (postStart > postEnd || inStart > inEnd) return null;
+        int val = postorder[postStart];
+        int inPos = inMap.get(val);
+        int rightTreeLen = inEnd - inPos;
+        TreeNode right = buildFromPostorderInorder(postorder, postStart + 1, postEnd + rightTreeLen,
+                inPos + 1, inEnd, inMap);
+        TreeNode left = buildFromPostorderInorder(postorder, postStart + rightTreeLen + 1, postEnd,
+                inStart, inPos - 1, inMap);
+        return new TreeNode(val, left, right);
+    }
+
     public static void main(String[] args) {
         int[] preorder = {1,2,4,5,6,7,3,8,9,10};
         int[] inorder = {4,5,2,7,6,1,3,8,10,9};
@@ -41,7 +70,18 @@ public class RecoverBinaryTree {
         for (int i = 0; i < po.length; i ++) {
             po[i] = post.get(i).val;
         }
+        System.out.println("For pre-order and in-order:");
         System.out.println(Arrays.hashCode(postorder));
         System.out.println(Arrays.hashCode(po));
+        System.out.println("Now for post-order and in-order:");
+        root = new RecoverBinaryTree().recoverFromPostOrderAndInorder(postorder, inorder);
+        List<TreeNode> pre = new LinkedList<>();
+        TreeNode.preOrder(root, pre);
+        int[] pr = new int[pre.size()];
+        for (int i = 0; i < pr.length; i ++) {
+            pr[i] = pre.get(i).val;
+        }
+        System.out.println(Arrays.hashCode(preorder));
+        System.out.println(Arrays.hashCode(pr));
     }
 }
