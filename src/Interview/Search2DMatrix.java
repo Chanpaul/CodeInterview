4ef69a0b4ac24ff8a0bbb2f9f1d99f9d0d8b2f87
Yongyang Yu <yuyongyang800@gmail.com>
2015-09-06 00:21:05 -0400

    refactor search2dmatrix

--- src/Interview/Search2DMatrix.java
+++ src/Interview/Search2DMatrix.java
@@ -19,6 +19,20 @@ package Interview;
  * Given target = 20, return false.
  */
 public class Search2DMatrix {
+    public boolean search(int[][] matrix, int target) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (target == matrix[row][col]) return true;
+            else if (matrix[row][col] < target) {
+                row ++;
+            }
+            else {
+                col --;
+            }
+        }
+        return false;
+    }
+
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) {
             return false;
