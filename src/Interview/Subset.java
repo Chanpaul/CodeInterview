a890689ed37d334c9928b5a11bf3405f956cedc5
Yongyang Yu <yuyongyang800@gmail.com>
2015-09-30 11:51:48 -0400

    refactor subset

--- src/Interview/Subset.java
+++ src/Interview/Subset.java
@@ -12,12 +12,12 @@ public class Subset {
             return;
         }
         if (!rval.contains(x)) {
-            rval.add(new HashSet<Integer>(x));
+            rval.add(new HashSet<>(x));
         }
         Iterator<Integer> iter = x.iterator();
         while (iter.hasNext()) {
             Integer curr = iter.next();
-            HashSet<Integer> tmp = new HashSet<Integer>(x);
+            HashSet<Integer> tmp = new HashSet<>(x);
             tmp.remove(curr);
             generate(tmp, rval);
             tmp.add(curr);
@@ -25,11 +25,11 @@ public class Subset {
     }
 
     public static void main(String[] args) {
-        HashSet<Integer> x = new HashSet<Integer>();
+        HashSet<Integer> x = new HashSet<>();
         x.add(1);
         x.add(2);
         x.add(3);
-        HashSet<HashSet<Integer>> rval = new HashSet<HashSet<Integer>>();
+        HashSet<HashSet<Integer>> rval = new HashSet<>();
         generate(x, rval);
         System.out.println(rval.size());
     }
