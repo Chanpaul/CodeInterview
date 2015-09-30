2bc695ccb395f1572874dccb5fd33733bc4401ee
Yongyang Yu <yuyongyang800@gmail.com>
2015-09-30 13:28:31 -0400

    refactor subset

--- src/Interview/Subset.java
+++ src/Interview/Subset.java
@@ -1,36 +1,63 @@
 package Interview;
 
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Created by yongyangyu on 3/16/15.
+ * Given a set of distinct integers, nums, return all possible subsets.
+ * Elements in a subset must be in non-descending order.
+ * The solution set must not contain duplicate subsets.
  */
 public class Subset {
-    public static void generate(HashSet<Integer> x, HashSet<HashSet<Integer>> rval) {
-        if (x.size() == 0) {
-            return;
+    public List<List<Integer>> subset(int[] nums) {
+        // use binary representation for enumerating
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < (1 << n); i ++) {
+            List<Integer> cur = new ArrayList<>();
+            for (int j = 0; j < n; j ++) {
+                if (((1 << j) & i) != 0) {
+                    cur.add(nums[j]);
+                }
+            }
+            res.add(cur);
         }
-        if (!rval.contains(x)) {
-            rval.add(new HashSet<>(x));
+        return res;
+    }
+
+    /**
+     * Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+     * Elements in a subset must be in non-descending order.
+     * The solution set must not contain duplicate subsets.
+     */
+    public List<List<Integer>> subsetDup(int[] nums) {
+        int n = nums.length;
+        Set<List<Integer>> res = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < (1 << n); i ++) {
+            List<Integer> cur = new ArrayList<>();
+            for (int j = 0; j < n; j ++) {
+                if (((1 << j) & i) != 0) {
+                    cur.add(nums[j]);
+                }
+            }
+            if (!res.contains(cur)) {
+                res.add(cur);
+            }
         }
-        Iterator<Integer> iter = x.iterator();
-        while (iter.hasNext()) {
-            Integer curr = iter.next();
-            HashSet<Integer> tmp = new HashSet<>(x);
-            tmp.remove(curr);
-            generate(tmp, rval);
-            tmp.add(curr);
+        List<List<Integer>> result = new ArrayList<>();
+        for (List x : res) {
+            result.add(x);
         }
+        return result;
     }
 
     public static void main(String[] args) {
-        HashSet<Integer> x = new HashSet<>();
-        x.add(1);
-        x.add(2);
-        x.add(3);
-        HashSet<HashSet<Integer>> rval = new HashSet<>();
-        generate(x, rval);
-        System.out.println(rval.size());
+        int[] nums = {1,2,2};
+        List<List<Integer>> res = new Subset().subsetDup(nums);
+        for (List x : res) {
+            System.out.println(Arrays.toString(x.toArray()));
+        }
     }
 }
