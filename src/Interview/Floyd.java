ab8ad72b8fbce206366abc356d22679ce117d5bf
Yongyang Yu <yuyongyang800@gmail.com>
2015-10-04 18:49:22 -0400

    refactor Floyd algo for onsite question, more readable version

--- src/Interview/Floyd.java
+++ src/Interview/Floyd.java
@@ -7,134 +7,148 @@ import java.util.*;
  * Interview question from Medallia onsite.
  * Given a two dimensional array of characters, a string as input,
  * output the moving direction sequence to build the string.
+ * Use Unicode to display arrows, i.e., Left("\u2190"), Up("\u2191"),
+ * Right("\u2192"), Down("\u2193")
  */
 public class Floyd {
-
-    private Map<Character, List<Integer>> getMap(char board[][]) {
-        Map<Character, List<Integer>> charPos = new HashMap<>();
+    private Map<Character, int[]> initBoard(char board[][]) {
+        Map<Character, int[]> charPos = new HashMap<>();
         for (int i = 0; i < board.length; i ++) {
             for (int j = 0; j < board[0].length; j ++) {
-                List<Integer> pos = new LinkedList<>();
-                pos.add(i);
-                pos.add(j);
-                charPos.put(new Character(board[i][j]), pos);
+                charPos.put(board[i][j], new int[]{i, j});
             }
         }
         return charPos;
     }
 
-    private String direction(List<Integer> pos1, List<Integer> pos2) {
+    private String direction(int[] pos1, int[] pos2) {
         // two cells are not adjacent
-        if ((pos1.get(0) != pos2.get(0)) && (pos1.get(1) != pos2.get(1))) {
+        if ((pos1[0] != pos2[0]) && (pos1[1] != pos2[1])) {
             return null;
         }
-        else if (pos1.get(0) == pos2.get(0)) {
-            if (pos2.get(1) == pos1.get(1) + 1) {
-                return "Right";
+        else if (pos1[0] == pos2[0]) {
+            if (pos2[1] == pos1[1] + 1) {
+                return "\u2192"; // Right
             }
-            if (pos2.get(1) == pos1.get(1) - 1) {
-                return "Left";
+            if (pos2[1] == pos1[1] - 1) {
+                return "\u2190"; // Left
             }
         }
-        else if (pos1.get(1) == pos2.get(1)) {
-            if (pos2.get(0) == pos1.get(0) + 1) {
-                return "Down";
+        else if (pos1[1] == pos2[1]) {
+            if (pos2[0] == pos1[0] + 1) {
+                return "\u2193"; // Down
             }
-            if (pos2.get(0) == pos1.get(0) - 1) {
-                return "Up";
+            if (pos2[0] == pos1[0] - 1) {
+                return "\u2191"; // Up
             }
         }
         return null;
     }
 
-    public Map<List<Character>, List<String>> floyd(char board[][]) {
-        Map<Character, List<Integer>> charPos = getMap(board);
-        Map<List<Character>, List<String>> map = new HashMap<>();
-        List<Character> flattenBoard = new ArrayList<>();
-        for (int i = 0; i < board.length; i ++) {
-            for (int j = 0; j < board.length; j ++) {
-                flattenBoard.add(board[i][j]);
+    // perform Floyd-Warshall algo and return the PI map
+    // i.e., PI(char1, char2) = char3, shortest path from char1 to char2 by char3
+    // PI(i, j) = PI(i, j) if dij <= dik + dkj
+    // PI(i, j) = PI(j, k) otherwise
+    public Map<List<Character>, Character> floyd(char board[][]) {
+        Map<Character, int[]> charPos = initBoard(board);
+        Map<List<Character>, Character> parent = new HashMap<>();
+        Map<List<Character>, Integer> dist = new HashMap<>();
+        int m = board.length, n = board[0].length;
+        char[] flattenBoard = new char[m * n];
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j ++) {
+                flattenBoard[i*n + j] = board[i][j];
             }
         }
         // "for each node, set distance to itself to 0"
-        for (int i = 0; i < flattenBoard.size(); i ++) {
-            List<Character> key = new LinkedList<>();
-            key.add(flattenBoard.get(i));
-            key.add(flattenBoard.get(i));
-            List<String> val = new LinkedList<>();
-            val.add("");
-            map.put(key, val);
+        for (int i = 0; i < flattenBoard.length; i ++) {
+            List<Character> key = new ArrayList<>();
+            key.add(flattenBoard[i]);
+            key.add(flattenBoard[i]);
+            dist.put(key, 0);
+            parent.put(key, null);
         }
         // "set for connected edges"
-        for (int i = 0; i < flattenBoard.size(); i ++) {
-            for (int j = 0; j < flattenBoard.size(); j ++) {
+        for (int i = 0; i < flattenBoard.length; i ++) {
+            for (int j = 0; j < flattenBoard.length; j ++) {
                 if (i == j) {
                     continue;
                 }
-                String dir = direction(charPos.get(flattenBoard.get(i)), charPos.get(flattenBoard.get(j)));
+                String dir = direction(charPos.get(flattenBoard[i]), charPos.get(flattenBoard[j]));
+                List<Character> key = new LinkedList<>();
+                key.add(flattenBoard[i]);
+                key.add(flattenBoard[j]);
                 if (dir != null) {
-                    List<Character> key = new LinkedList<>();
-                    key.add(flattenBoard.get(i));
-                    key.add(flattenBoard.get(j));
-                    List<String> val = new LinkedList<>();
-                    val.add(dir);
-                    map.put(key, val);
+                    dist.put(key, 1);
+                    parent.put(key, flattenBoard[i]);
+                }
+                else {
+                    dist.put(key, Integer.MAX_VALUE);
+                    parent.put(key, null);
                 }
             }
         }
         // "set indirect edges"
-        for (int k = 0; k < flattenBoard.size(); k ++) {
-            for (int i = 0; i < flattenBoard.size(); i ++) {
-                for (int j = 0; j < flattenBoard.size(); j ++) {
-                    List<Character> key = new LinkedList<>();
-                    key.add(flattenBoard.get(i));
-                    key.add(flattenBoard.get(j));
-                    List<String> val = null;
-                    if (map.containsKey(key)) {
-                        val = map.get(key);
-                    }
-                    List<Character> key1 = new LinkedList<>();
-                    List<Character> key2 = new LinkedList<>();
-                    key1.add(flattenBoard.get(i));
-                    key1.add(flattenBoard.get(k));
-                    key2.add(flattenBoard.get(k));
-                    key2.add(flattenBoard.get(j));
-                    if (map.containsKey(key1) && map.containsKey(key2)) {
-                        List<Character> newKey = new LinkedList<>();
-                        newKey.add(flattenBoard.get(i));
-                        newKey.add(flattenBoard.get(j));
-                        List<String> newVal = new LinkedList<>();
-                        newVal.addAll(map.get(key1));
-                        newVal.addAll(map.get(key2));
-                        if (val == null) {
-                            map.put(newKey, newVal);
-                        }
-                        else {
-                            if (newVal.size() < val.size()) {
-                                map.put(newKey, newVal);
-                            }
+        for (int k = 0; k < flattenBoard.length; k ++) {
+            for (int i = 0; i < flattenBoard.length; i ++) {
+                for (int j = 0; j < flattenBoard.length; j ++) {
+                    List<Character> key = new ArrayList<>();
+                    key.add(flattenBoard[i]);
+                    key.add(flattenBoard[j]);
+                    int dij = dist.get(key);
+                    key = new ArrayList<>();
+                    key.add(flattenBoard[i]);
+                    key.add(flattenBoard[k]);
+                    int dik = dist.get(key);
+                    key = new ArrayList<>();
+                    key.add(flattenBoard[k]);
+                    key.add(flattenBoard[j]);
+                    int dkj = dist.get(key);
+                    if (dik == Integer.MAX_VALUE || dkj == Integer.MAX_VALUE) continue;
+                    else {
+                        if (dij > dik + dkj) {
+                            key = new ArrayList<>();
+                            key.add(flattenBoard[i]);
+                            key.add(flattenBoard[j]);
+                            dist.put(key, dik + dkj);
+                            List<Character> key2 = new ArrayList<>();
+                            key2.add(flattenBoard[k]);
+                            key2.add(flattenBoard[j]);
+                            parent.put(key, parent.get(key2));
                         }
                     }
                 }
             }
         }
-        return map;
+        return parent;
     }
 
     public List<String> path(char board[][], char start, String target) {
+        Map<Character, int[]> char2pos = initBoard(board);
         List<String> res = new LinkedList<>();
-        Map<List<Character>, List<String>> map = floyd(board);
-        List<Character> curr = new LinkedList<>();
-        curr.add(start);
-        curr.add(target.charAt(0));
-        res.addAll(map.get(curr));
-        res.add("OK");
-        for (int i = 1; i < target.length(); i ++) {
-            curr.remove(0);
-            curr.add(target.charAt(i));
-            res.addAll(map.get(curr));
-            res.add("OK");
+        Map<List<Character>, Character> parent = floyd(board);
+        for (int i = 0; i < target.length(); i ++) {
+            res.addAll(getPath(char2pos, parent, start, target.charAt(i)));
+            start = target.charAt(i);
         }
+        return res;
+    }
+
+    private List<String> getPath(Map<Character, int[]>char2pos, Map<List<Character>, Character> parent,
+                                 char from, char to) {
+        List<String> res = new LinkedList<>();
+        List<Character> key = new ArrayList<>();
+        key.add(from);
+        key.add(to);
+        res.add("OK");
+        while (parent.get(key) != from) {
+            char middle = parent.get(key);
+            res.add(0, direction(char2pos.get(middle), char2pos.get(to)));
+            to = middle;
+            key.remove(key.size() - 1);
+            key.add(to);
+        }
+        res.add(0, direction(char2pos.get(from), char2pos.get(to)));
         return res;
     }
 
@@ -144,7 +158,7 @@ public class Floyd {
                           {'K', 'L', 'M', 'N', 'O'},
                           {'P', 'Q', 'R', 'S', 'T'},
                           {'U', 'V', 'W', 'X', 'Y'}};
-        String str = "AYOR";
+        String str = "MATRIX";
         char start = 'A';
         Floyd f = new Floyd();
         List<String> route = f.path(board, start, str);
