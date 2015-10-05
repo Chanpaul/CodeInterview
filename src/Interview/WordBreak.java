fafb571f865586423e908583b12085fb671376f4
Yongyang Yu <yuyongyang800@gmail.com>
2015-10-05 13:46:58 -0400

    update for word break I

--- src/Interview/WordBreak.java
+++ src/Interview/WordBreak.java
@@ -4,84 +4,43 @@ import java.util.*;
 
 /**
  * Created by yongyangyu on 1/21/15.
- * Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where
- * each word is a valid dictionary word.
- *
- * Return all such possible sentences.
+ * Given a string s and a dictionary of words dict, determine if s
+ * can be segmented into a space-separated sequence of one or more dictionary words.
  *
  * For example, given
- * s = "catsanddog",
- * dict = ["cat", "cats", "and", "sand", "dog"].
+ * s = "leetcode",
+ * dict = ["leet", "code"].
  *
- * A solution is ["cats and dog", "cat sand dog"].
+ * Return true because "leetcode" can be segmented as "leet code".
  */
 public class WordBreak {
-    public static List<String> wordBreak(String s, Set<String> dict) {
-        List<String> rval = null;
-        Map<Integer, LinkedList<LinkedList<String>>> map = new HashMap<>();
-        if (s.length() == 0 && dict.size() == 0) {
-            rval = new LinkedList<>();
-            rval.add("");
-        }
-        if (dict.contains(String.valueOf(s.charAt(0)))) {
-            LinkedList<LinkedList<String>> tmp = new LinkedList<>();
-            tmp.add(new LinkedList<>());
-            tmp.get(0).add(String.valueOf(s.charAt(0)));
-            map.put(0, tmp);
-        }
+    public boolean wordBreak1(String s, Set<String> wordDict) {
+        boolean[] dp = new boolean[s.length()];
+        if (wordDict.contains(s)) return true;
+        if (s.length() == 0 && wordDict.size() == 0) return true;
+        if (s.length() == 0 && wordDict.size() != 0) return false;
+        if (wordDict.contains(s.substring(0, 1))) dp[0] = true;
         for (int i = 1; i < s.length(); i ++) {
-            if (dict.contains(s.substring(0, i + 1))) {
-                LinkedList<LinkedList<String>> tmp = new LinkedList<>();
-                tmp.add(new LinkedList<>());
-                tmp.get(0).add(s.substring(0, i + 1));
-                map.put(i, tmp);
+            if (wordDict.contains(s.substring(0, i + 1))) {
+                dp[i] = true;
+                continue;
             }
-            for (int j = i-1; j >= 0; j --) {
-                if (dict.contains(s.substring(j+1, i+1)) && map.containsKey(j)) {
-                    LinkedList<LinkedList<String>> tmp = new LinkedList<>(map.get(j));
-                    for (int k = 0; k < tmp.size(); k ++) {
-                        tmp.get(k).add(s.substring(j+1, i + 1));
-                    }
-                    LinkedList<LinkedList<String>> curr = null;
-                    if (map.containsKey(i)) {
-                        curr = map.get(i);
-                    }
-                    if (curr == null) {
-                        curr = new LinkedList<>();
-                    }
-                    for (LinkedList<String> x : tmp) {
-                        curr.add(x);
-                    }
-                    map.put(i, curr);
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && wordDict.contains(s.substring(j+1, i+1))) {
+                    dp[i] = true;
                 }
             }
         }
-        if (map.containsKey(s.length() - 1)) {
-            rval = new LinkedList<>();
-            for (LinkedList<String> x : map.get(s.length() - 1)) {
-                StringBuilder sb = new StringBuilder();
-                sb.append(x.get(0));
-                for (int n = 1; n < x.size(); n ++) {
-                    sb.append(' ');
-                    sb.append(x.get(n));
-                }
-                rval.add(sb.toString());
-            }
-        }
-        return rval;
+        return dp[s.length() - 1];
     }
 
     public static void main(String[] args) {
-        String s = "catsanddog";
+        String s = "leetcode";
         Set<String> dict = new HashSet<>();
-        dict.add("cat");
-        dict.add("cats");
-        dict.add("and");
-        dict.add("sand");
-        dict.add("dog");
-        List<String> res = wordBreak(s, dict);
-        for (String x : res) {
-            System.out.println(x);
+        String[] d = {"leet", "code"};
+        for (String word: d) {
+            dict.add(word);
         }
+       System.out.println(new WordBreak().wordBreak1(s, dict));
     }
 }
