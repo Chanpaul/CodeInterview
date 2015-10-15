b144436c41daf6537396d1292f7c9853f0365022
Yongyang Yu <yuyongyang800@gmail.com>
2015-10-15 16:04:41 -0400

    update combine sumIII

--- src/Interview/CombineSumIII.java
+++ src/Interview/CombineSumIII.java
@@ -7,25 +7,15 @@ import java.util.List;
  * Find all possible combinations of k numbers that add up to a number n, 
  * given that only numbers from 1 to 9 can be used and each combination should be 
  * a unique set of numbers.
-
  * Ensure that numbers within the set are sorted in ascending order.
-
-
  * Example 1:
-
- * Input: k = 3, n = 7
-
- * Output:
-
- * [[1,2,4]]
-
+ *  Input: k = 3, n = 7
+ *  Output:
+ *      [[1,2,4]]
  * Example 2:
-
- * Input: k = 3, n = 9
-
- * Output:
-
- * [[1,2,6], [1,3,5], [2,3,4]]
+ *  Input: k = 3, n = 9
+ *  Output:
+ *      [[1,2,6], [1,3,5], [2,3,4]]
  */
 public class CombineSumIII {
     public static List<List<Integer>> combinationSum3(int k, int n) {
