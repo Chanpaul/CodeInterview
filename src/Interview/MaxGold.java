94cfd54d11e406e0d641bb57d28ad9228d295187
Yongyang Yu <yuyongyang800@gmail.com>
2015-10-30 14:16:05 -0400

    add comments for max gold

--- src/Interview/MaxGold.java
+++ src/Interview/MaxGold.java
@@ -2,6 +2,8 @@ package Interview;
 
 /**
  * Created by yongyangyu on 11/23/14.
+ * Give a line of gold on the table, two players take turns to pick one piece from
+ * either end of the line. Compute the max gold one can get if he start first.
  */
 public class MaxGold {
     public static int getMaxGold(int[] A, int l, int r, boolean firstPlay) {
