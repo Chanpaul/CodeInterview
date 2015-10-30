c88441ed39538ee2466c312059677a8f28d37b01
Yongyang Yu <yuyongyang800@gmail.com>
2015-10-30 14:37:22 -0400

    change description

--- src/Interview/RegularExpressMatching.java
+++ src/Interview/RegularExpressMatching.java
@@ -3,23 +3,23 @@ package Interview;
 /**
  * Created by yongyangyu on 6/24/15.
  * Implement regular expression matching with support for '.' and '*'.
-
- '.' Matches any single character.
- '*' Matches zero or more of the preceding element.
-
- The matching should cover the entire input string (not partial).
-
- The function prototype should be:
- bool isMatch(const char *s, const char *p)
-
- Some examples:
- isMatch("aa","a") → false
- isMatch("aa","aa") → true
- isMatch("aaa","aa") → false
- isMatch("aa", "a*") → true
- isMatch("aa", ".*") → true
- isMatch("ab", ".*") → true
- isMatch("aab", "c*a*b") → true
+ *
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ *
+ * The matching should cover the entire input string (not partial).
+ *
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ *
+ * Some examples:
+ * isMatch("aa","a") -> false
+ * isMatch("aa","aa") -> true
+ * isMatch("aaa","aa") -> false
+ * isMatch("aa", "a*") -> true
+ * isMatch("aa", ".*") -> true
+ * isMatch("ab", ".*") -> true
+ * isMatch("aab", "c*a*b") -> true
  */
 public class RegularExpressMatching {
     // String s is the candidate string, string p is a pattern string with '.' and '*';
