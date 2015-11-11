6d1afdc786351d370c6fd3b9a715a67b3d176ab9
Yongyang Yu <yuyongyang800@gmail.com>
2015-11-11 15:01:21 -0500

    add shortest word distance I

---
+++ src/Interview/ShortestWordDistance.java
@@ -0,0 +1,37 @@
+package Interview;
+
+/**
+ * Created by yongyangyu on 11/11/15.
+ * Given a list of words and two words word1 and word2, return the shortest distance between
+ * these two words in the list.
+ *
+ * For example,
+ * Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+ *
+ * Given word1 = "coding", word2 = "practice", return 3.
+ * Given word1 = "makes", word2 = "coding", return 1.
+ *
+ * Note:
+ * You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+ */
+public class ShortestWordDistance {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        int dist = Integer.MAX_VALUE;
+        int idx1 = -1, idx2 = -1;
+        for (int i = 0; i < words.length; i ++) {
+            if (word1.equals(words[i])) idx1 = i;
+            else if (word2.equals(words[i])) idx2 = i;
+            if (idx1 != -1 && idx2 != -1) {
+                dist = Math.min(dist, Math.abs(idx1 - idx2));
+            }
+        }
+        return dist;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"practice", "makes", "perfect", "coding", "makes"};
+        String word1 = "makes", word2 = "coding";
+        ShortestWordDistance swd = new ShortestWordDistance();
+        System.out.println(swd.shortestDistance(words, word1, word2));
+    }
+}
