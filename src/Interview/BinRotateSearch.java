633c98add55d2ceead1558a0e994dd6cbeebc000
Yongyang Yu <yuyongyang800@gmail.com>
2015-11-12 16:28:16 -0500

    refactor binary rotate search

--- src/Interview/BinRotateSearch.java
+++ src/Interview/BinRotateSearch.java
@@ -10,58 +10,58 @@ package Interview;
  *
  * You may assume no duplicate exists in the array.
  */
-public class RotateSearch {
+public class BinRotateSearch {
     public int search(int[] nums, int target) {
         if (nums == null) return -1;
         int lo = 0, hi = nums.length-1;
         while (lo <= hi) {
             int mid = lo + (hi-lo)/2;
             if (nums[mid] == target) return mid;
-            if (nums[lo] <= nums[mid]) {
-                if (nums[lo] <= target && target < nums[mid])
+            if (nums[lo] <= nums[mid]) { // first half in increasing order
+                if (nums[lo] <= target && target < nums[mid]) // in first half
                     hi = mid - 1;
-                else
+                else // in second half
                     lo = mid + 1;
             }
-            else {
-                if (nums[mid] < target && target <= nums[hi]) {
+            else { // second half in increasing order
+                if (nums[mid] < target && target <= nums[hi]) { // in second half
                     lo = mid + 1;
                 }
-                else
+                else // in first half
                     hi = mid - 1;
             }
         }
         return -1;
     }
-    public static boolean searchDup(int[] A, int target) {
-        int l = 0; int r = A.length - 1;
-        while (l <= r) {
-            int mid = l + (r-l)/2;
-            if (target == A[mid]) {
+    public static boolean searchDup(int[] nums, int target) {
+        int lo = 0; int hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if (target == nums[mid]) {
                 return true;
             }
-            if (A[l] == A[mid] || A[l] == A[r]) {
-                l ++;
+            if (nums[lo] == nums[mid] || nums[lo] == nums[hi]) {
+                lo ++;
                 continue;
             }
-            if (A[mid] == A[r]) {
-                r --;
+            if (nums[mid] == nums[hi]) {
+                hi --;
                 continue;
             }
-            if (A[l] < A[mid]) {
-                if (target > A[mid] || target < A[l]) {
-                    l = mid + 1;
+            if (nums[lo] < nums[mid]) {
+                if (target > nums[mid] || target < nums[lo]) {
+                    lo = mid + 1;
                 }
                 else {
-                    r = mid - 1;
+                    hi = mid - 1;
                 }
             }
-            else if (A[l] > A[mid]) {
-                if (target >= A[l]) {
-                    r = mid;
+            else if (nums[lo] > nums[mid]) {
+                if (target >= nums[lo]) {
+                    hi = mid;
                 }
-                else if (target >= A[mid]) {
-                    l = mid;
+                else if (target >= nums[mid]) {
+                    hi = mid;
                 }
                 else {
                     return false;
