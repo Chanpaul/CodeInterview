96f18e49d8109be1f9fc42e3c2bf641b76aa23af
Yongyang Yu <yuyongyang800@gmail.com>
2015-11-13 21:49:25 -0500

    update word pattern follow-up question

--- src/Interview/WordPattern.java
+++ src/Interview/WordPattern.java
@@ -1,7 +1,9 @@
 package Interview;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by yongyangyu on 10/5/15.
@@ -36,6 +38,48 @@ public class WordPattern {
         return true;
     }
 
+    /*
+     * follow-up question:
+     * Given a pattern and a string str, find if str follows the same pattern.
+     *
+     * Here follow means a full match, such that there is a bijection between a letter
+     * in pattern and a non-empty substring in str.
+     *
+     * Examples:
+     *
+     * pattern = "abab", str = "redblueredblue" should return true.
+     * pattern = "aaaa", str = "asdasdasdasd" should return true.
+     * pattern = "aabb", str = "xyzabcxzyabc" should return false.
+     */
+    public boolean wordPatternMatch(String pattern, String str) {
+        Map<Character, String> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        return isMatch(str, 0, pattern, 0, map, set);
+    }
+
+    private boolean isMatch(String str, int i, String pat, int j,
+                            Map<Character, String> map, Set<String> set) {
+        if (i == str.length() && j == pat.length()) return true;
+        if (i == str.length() || j == pat.length()) return false;
+        char c = pat.charAt(j);
+        if (map.containsKey(c)) {
+            String s = map.get(c);
+            // check if we can use it to match str[i, ..., i+s.length()]
+            if (!str.startsWith(s, i)) return false;
+            return isMatch(str, i+s.length(), pat, j+1, map, set);
+        }
+        for (int k = i; i < str.length(); k ++) {
+            String p = str.substring(i, k+1);
+            if (set.contains(p)) continue;
+            map.put(c, p);
+            set.add(p);
+            if (isMatch(str, k+1, pat, j+1, map, set)) return true;
+            map.remove(c);
+            set.remove(p);
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         String[] patterns = {"abba", "abba", "aaaa", "abba"};
         String[] strs = {"dog cat cat dog", "dog cat cat fish",
@@ -44,5 +88,6 @@ public class WordPattern {
         for (int i = 0; i < patterns.length; i ++) {
             System.out.println(wp.wordPattern(patterns[i], strs[i]));
         }
+        System.out.println(wp.wordPatternMatch("abab", "redblueredblue"));
     }
 }
