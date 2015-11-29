f75fe9ad7cc3a3f87e6048a9149058450c560c91
Yongyang Yu <yuyongyang800@gmail.com>
2015-11-29 17:37:18 -0500

    add dp solution

--- src/BurstBallons.java
+++ src/BurstBallons.java
@@ -41,9 +41,27 @@ public class BurstBallons {
         return res;
     }
 
+    public int maxCoins2(int[] nums) {
+        int[] arr = new int[nums.length+2];
+        int n = 1;
+        for (int x: nums) arr[n++] = x;
+        arr[0] = arr[n++] = 1;
+        int[][] dp = new int[n][n];
+        for (int k = 2; k < n; k ++) {
+            for (int left = 0; left < n - k; left ++) {
+                int right = left + k;
+                for (int i = left+1; i < right; i ++) {
+                    dp[left][right] = Math.max(dp[left][right],
+                            arr[left] * arr[i] * arr[right] + dp[left][i] + dp[i][right]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
+
     public static void main(String[] args) {
         int[] nums = {3,1,5,8};
         BurstBallons bb = new BurstBallons();
-        System.out.println(bb.maxCoins(nums));
+        System.out.println(bb.maxCoins2(nums));
     }
 }
