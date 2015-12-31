b6fdfc1ce64e5b96ca0aeed224f8e95a879d7212
Yongyang Yu <yuyongyang800@gmail.com>
2015-12-31 17:20:36 -0500

    update a new method for wiggle sort

--- src/Interview/WiggleSort.java
+++ src/Interview/WiggleSort.java
@@ -1,8 +1,6 @@
 package Interview;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * Created by yongyangyu on 11/24/14.
@@ -13,35 +11,36 @@ import java.util.List;
  * Note: You should solve it in place and one pass.
  */
 public class WiggleSort {
-    public static void swap(List<Integer> A, int i, int j) {
-        int tmp = A.get(i);
-        A.set(i, A.get(j));
-        A.set(j, tmp);
+    public static void swap(int[] A, int i, int j) {
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
     }
 
-    public static void rearrange(List<Integer> A) {
-        for (int i = 1; i < A.size(); i ++){
-            if (((i % 2 == 1) && A.get(i-1) > A.get(i) || ((i % 2 == 0) && A.get(i-1) < A.get(i)))) {
+    public static void wsort(int[] A) {
+        for (int i = 1; i < A.length; i ++){
+            if (((i % 2 == 1) && A[i-1] > A[i] || ((i % 2 == 0) && A[i-1] < A[i]))) {
                 swap(A, i-1, i);
             }
         }
     }
 
+    // a[0] < a[1] > a[2] < a[3] >...
+    // equal numbers should not be adjacent to each other
+    public static void wsort2(int[] A) {
+        Arrays.sort(A);
+        int[] B = new int[A.length];
+        int i = (A.length-1) / 2, j = A.length - 1;
+        for (int k = 0; k < B.length; k ++) {
+            if (k % 2 == 0) B[k] = A[i--];
+            else B[k] = A[j--];
+        }
+        System.arraycopy(B, 0, A, 0, A.length);
+    }
+
     public static void main(String[] args) {
-        int [] A = {1,2,3,4,5,6,7,8,9,10};
-        List<Integer> B = new ArrayList<Integer>();
-        for (int x : A) {
-            B.add(x);
-        }
-        Collections.shuffle(B);
-        for (Integer x : B) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
-        rearrange(B);
-        for (Integer x : B) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
+        int [] A = {4,5,5,6};
+        wsort2(A);
+        System.out.println(Arrays.toString(A));
     }
 }
