a88eaff64ce7539d9f02a12593295c3ff3f970e9
Yongyang Yu <yuyongyang800@gmail.com>
2016-02-23 18:29:41 -0500

    format comments

--- src/Interview/SeflCrossing.java
+++ src/Interview/SeflCrossing.java
@@ -40,12 +40,12 @@ public class SeflCrossing {
     public boolean isSelfCrossing(int[] x) {
         for (int i = 3; i < x.length; i++) {
             // Case 1: current line crosses the line 3 steps ahead of it
-            if (x[i]>=x[i-2] && x[i-1]<=x[i-3]) return true;
+            if (x[i] >= x[i-2] && x[i-1] <= x[i-3]) return true;
             // Case 2: current line crosses the line 4 steps ahead of it
-            else if (i>=4 && x[i-1]==x[i-3] && x[i]+x[i-4]>=x[i-2]) return true;
+            else if (i >= 4 && x[i-1] == x[i-3] && x[i] + x[i-4] >= x[i-2]) return true;
             // Case 3: current line crosses the line 6 steps ahead of it
-            else if (i>=5 && x[i-2]>=x[i-4] && x[i]+x[i-4]>=x[i-2]
-                    && x[i-1]<=x[i-3] && x[i-1]+x[i-5]>=x[i-3]) return true;
+            else if (i >= 5 && x[i-2] >= x[i-4] && x[i] + x[i-4] >= x[i-2]
+                    && x[i-1] <= x[i-3] && x[i-1] + x[i-5] >= x[i-3]) return true;
         }
         return false;
     }
